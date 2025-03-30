c7f973884f9350133b2188fbec6f3c0f9e430793
Mahesh <119264975+mahesh-m1@users.noreply.github.com>
2025-03-30 17:13:09 +0530

    Update math.groovy

--- vars/math.groovy
+++ vars/math.groovy
@@ -1,1 +1,16 @@
-
+def(a,b)
+{
+   echo "The sum of ${a} and ${b} is ${a+b}
+}
+def(a,b)
+{
+   echo "The diff of ${a} and ${b} is ${a-b}
+}
+def(a,b)
+{
+   echo "The product of ${a} and ${b} is ${a*b}
+}
+def(a,b)
+{
+   echo "The div of ${a} and ${b} is ${a/b}
+}
