c88acdfff8be885124553d34fd31fa4766ad274e
Mahesh <119264975+mahesh-m1@users.noreply.github.com>
2025-03-31 00:09:34 +0530

    Update cicd.groovy

--- vars/cicd.groovy
+++ vars/cicd.groovy
@@ -10,3 +10,7 @@ def tomcatDeploy(jobname,ip,context)
 {
   sh "scp /var/lib/jenkins/workspace/${jobname}/webapp/target/webapp.war ubuntu@${ip}:/var/lib/tomcat10/webapps/${context}.war"
 }
+def executeSelinium(jobname)
+{
+ sh "java -jar /var/lib/jenkins/workspace/${jobname}/testing.jar"
+}
